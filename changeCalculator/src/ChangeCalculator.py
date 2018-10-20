6eaa58aafda969b23fc8893f759bf2dd6cf9ccea
Jamie <jamie.meldrum@infinityworks.com>
2018-10-20 15:02:09 +0100

    Added more complete test coverage

--- changeCalculator/src/ChangeCalculator.py
+++ changeCalculator/src/ChangeCalculator.py
@@ -2,6 +2,13 @@ class ChangeCalculator:
     coins = [200, 100, 50, 20, 10, 5, 2, 1]
 
     def calculateSmallestChange(self, total):
+
+        if (total < 0):
+            raise ValueError("Total must be 0 or greater")
+
+        if (total > 4999):
+            raise ValueError("Total must not be greater than 4999")
+
         change = []
         while total > 0:
             for coin in self.coins:
--- changeCalculator/test/ChangeCalculatorTest.py
+++ changeCalculator/test/ChangeCalculatorTest.py
@@ -5,30 +5,46 @@ from ChangeCalculator import ChangeCalculator
 
 class ChangeCalculatorTest(unittest.TestCase):
 
-    #Test out of bounds
-    #Test zero
+    if __name__ == '__main__':
+        unittest.main()
+
+    changeCalculator = ChangeCalculator()
+
+    def test_get_change_out_of_range(self):
+        with self.assertRaises(ValueError) as context:
+            self.changeCalculator.calculateSmallestChange(5000)
+
+        self.assertTrue("Total must not be greater than 4999" in str(context.exception))
+
+    def test_get_change_Negative_input(self):
+        with self.assertRaises(ValueError) as context:
+            self.changeCalculator.calculateSmallestChange(-1)
+
+        self.assertTrue("Total must be 0 or greater" in str(context.exception))
+
+    def test_get_change_zero(self):
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(0) == [])
 
     def test_get_change_single_coin(self):
-        changeCalculator = ChangeCalculator()
-        self.assertTrue(changeCalculator.calculateSmallestChange(1) == [1])
-        self.assertTrue(changeCalculator.calculateSmallestChange(2) == [2])
-        self.assertTrue(changeCalculator.calculateSmallestChange(5) == [5])
-        self.assertTrue(changeCalculator.calculateSmallestChange(10) == [10])
-        self.assertTrue(changeCalculator.calculateSmallestChange(20) == [20])
-        self.assertTrue(changeCalculator.calculateSmallestChange(50) == [50])
-        self.assertTrue(changeCalculator.calculateSmallestChange(100) == [100])
-        self.assertTrue(changeCalculator.calculateSmallestChange(200) == [200])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(1) == [1])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(2) == [2])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(5) == [5])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(10) == [10])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(20) == [20])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(50) == [50])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(100) == [100])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(200) == [200])
 
     def test_get_change_multiple_coins(self):
-        changeCalculator = ChangeCalculator()
-        self.assertTrue(changeCalculator.calculateSmallestChange(3) == [2,1])
-        self.assertTrue(changeCalculator.calculateSmallestChange(4) == [2,2])
-        self.assertTrue(changeCalculator.calculateSmallestChange(6) == [5,1])
-        self.assertTrue(changeCalculator.calculateSmallestChange(17) == [10,5,2])
-        self.assertTrue(changeCalculator.calculateSmallestChange(33) == [20,10,2,1])
-        self.assertTrue(changeCalculator.calculateSmallestChange(84) == [50,20,10,2,2])
-        self.assertTrue(changeCalculator.calculateSmallestChange(167) == [100,50,10,5,2])
-        self.assertTrue(changeCalculator.calculateSmallestChange(465) == [200,200,50,10,5])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(3) == [2,1])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(4) == [2,2])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(6) == [5,1])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(17) == [10,5,2])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(33) == [20,10,2,1])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(84) == [50,20,10,2,2])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(167) == [100,50,10,5,2])
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(465) == [200,200,50,10,5])
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_get_change_upper_boundary(self):
+        self.assertTrue(self.changeCalculator.calculateSmallestChange(4999) == [200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,100,50,20,20,5,2,2])
+
